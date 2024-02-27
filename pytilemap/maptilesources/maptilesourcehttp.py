76fb122c859a6a0ba3bc5cd4599cbe9d16bf42f1
Fletcher Walmsley <fletcherpw99@gmail.com>
2024-02-27 16:33:13 +1300

    Don't invalidate tiles when changing zoom levels.
    Speeds up map display when moving between levels

--- pytilemap/maptilesources/maptilesourcehttp.py
+++ pytilemap/maptilesources/maptilesourcehttp.py
@@ -5,8 +5,6 @@ from qtpy.QtGui import QPixmap
 
 from .maptilesource import MapTileSource
 import grequests
-import requests
-import os
 
 
 class MapTileHTTPLoader(QObject):
@@ -18,68 +16,54 @@ class MapTileHTTPLoader(QObject):
         self._tileInDownload = dict()
         self._grs = list()
         self._gps_keys = list()
-
-    @Slot(int, int, int, str)
-    def loadTile(self, x, y, zoom, url):
-
-        url = f"https://basemaps.linz.govt.nz/v1/tiles/aerial/WebMercatorQuad/{zoom}/{x}/{y}.webp?api=c01hj0qr6shwmem3jazjgqvrzsc"
-        key = (x, y, zoom)
-        if key not in self._tileInDownload:
-            # Request the image to the map service
-            print(f"Requesting: {url}")
-            self._tileInDownload[key] = requests.get(url).content
-            # base_path = os.path.join(f"{zoom}/{x}")
-            # if not os.path.exists(base_path):
-            #     os.makedirs(base_path, exist_ok=True)
-            #
-            # with open(f"{base_path}/{y}.png", mode="wb") as f:
-            #     f.write(self._tileInDownload[key])
-            #     print(f"Saved {base_path} to disk")
-
-        self.tileLoaded.emit(x, y, zoom, self._tileInDownload[key])
+        self._local_keys = list()
 
     @Slot(int, int, int, str)
     def asyncLoadTile(self, x, y, zoom, url):
         url = f"https://basemaps.linz.govt.nz/v1/tiles/aerial/WebMercatorQuad/{zoom}/{x}/{y}.jpeg?api=c01hj0qr6shwmem3jazjgqvrzsc"
         key = (x, y, zoom)
         if key not in self._tileInDownload:
-            # Request the image to the map service
-            print(f"Adding: {url} to request que")
             self._grs.append(grequests.get(url))
             self._gps_keys.append(key)
+        else:
+            self.tileLoaded.emit(key[0], key[1], key[2], self._tileInDownload[key])
+
 
 
     @Slot()
     def asyncFetchTile(self):
-        responses = grequests.map(self._grs)
-        for key, response in zip(self._gps_keys, responses):
-            self._tileInDownload[key] = response.content
-            self.tileLoaded.emit(key[0], key[1], key[2], self._tileInDownload[key])
+        # Return the requested images from remote server
+        if len(self._grs) > 0:
+            responses = grequests.map(self._grs)
+            for key, response in zip(self._gps_keys, responses):
+                self._tileInDownload[key] = response.content
+                self.tileLoaded.emit(key[0], key[1], key[2], self._tileInDownload[key])
+            self._grs.clear()
+            self._gps_keys.clear()
 
-        self._grs.clear()
-        self._gps_keys.clear()
 
     @Slot()
     def abortRequest(self, x, y, zoom):
-        p = (x, y, zoom)
-        if p in self._tileInDownload:
+        # p = (x, y, zoom)
+        # if p in self._tileInDownload:
             # reply = self._tileInDownload[p]
-            del self._tileInDownload[p]
+            # del self._tileInDownload[p]
         #     reply.close()
         #     reply.deleteLater()
         print("Aborting requests")
-        self._grs.clear()
-        self._gps_keys.clear()
+        # self._grs.clear()
+        # self._gps_keys.clear()
 
     @Slot()
     def abortAllRequests(self):
-        for x, y, zoom in list(self._tileInDownload.keys()):
-            self.abortRequest(x, y, zoom)
+        # for x, y, zoom in list(self._tileInDownload.keys()):
+        #     self.abortRequest(x, y, zoom)
+        pass
 
 
 class MapTileSourceHTTP(MapTileSource):
 
-    def __init__(self, tileSize=256, minZoom=2, maxZoom=20, mapHttpLoader=None, parent=None):
+    def __init__(self, tileSize=256, minZoom=2, maxZoom=21, mapHttpLoader=None, parent=None):
         MapTileSource.__init__(self, tileSize=tileSize, minZoom=minZoom, maxZoom=maxZoom, parent=parent)
 
         if mapHttpLoader is not None:
